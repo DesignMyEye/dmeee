db1713f38b304b0b7b71ea82a095ad40632ca4ce
kilaws444 <kilaws44@mail.com>
2018-09-05 02:15:52 -0300

    init

---
+++ src/app/presentation/presentation.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, OnDestroy, HostListener, ElementRef } from '@angular/core';
+
+declare const $: any;
+
+
+@Component({
+    selector: 'app-presentation',
+    templateUrl: './presentation.component.html',
+    styleUrls: ['./presentation.component.scss']
+})
+
+export class PresentationComponent implements OnInit, OnDestroy {
+    model = {
+        left: true,
+        middle: false,
+        right: false
+    };
+    date: Date = new Date();
+
+     constructor(public el: ElementRef) { }
+     @HostListener('window:scroll', ['$event'])
+     checkScroll() {
+        const componentPosition = document.getElementsByClassName('add-animation');
+        const scrollPosition = window.pageYOffset; 
+        for(var i = 0; i < componentPosition.length; i++) {
+            var rec = componentPosition[i].getBoundingClientRect().top + window.scrollY + 100;
+            if ( scrollPosition + window.innerHeight >= rec ) {
+                componentPosition[i].classList.add('animated');
+            } else if ( scrollPosition + window.innerHeight * 0.8 < rec ) {
+                componentPosition[i].classList.remove('animated');
+            }
+        }
+     }
+    ngOnInit() {
+        var body = document.getElementsByTagName('body')[0];
+        body.classList.add('presentation-page');
+        body.classList.add('loading');
+        var navbar = document.getElementsByTagName('nav')[0];
+        navbar.classList.add('navbar-transparent');
+    }
+    ngOnDestroy(){
+        var body = document.getElementsByTagName('body')[0];
+        body.classList.remove('presentation-page');
+        body.classList.remove('loading');
+        var navbar = document.getElementsByTagName('nav')[0];
+        navbar.classList.remove('navbar-transparent');
+    }
+}
---
+++ src/app/sections/contacts/mail.php
@@ -0,0 +1,36 @@
+﻿<?php
+
+// Guardar los datos recibidos en variables:
+$nombre = $_POST['name'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+// Definir el correo de destino:
+$dest = "depetrinicolas@gmail.com"; 
+ 
+// Estas son cabeceras que se usan para evitar que el correo llegue a SPAM:
+$headers = "From: $nombre <$email>\r\n";  
+$headers .= "X-Mailer: PHP7\n";
+$headers .= 'MIME-Version: 1.0' . "\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+ 
+// Aqui definimos el asunto y armamos el cuerpo del mensaje
+$asunto = "Contacto";
+$cuerpo = "Nombre: ".$nombre."<br>";
+$cuerpo .= "Email: ".$email."<br>";
+$cuerpo .= "Mensaje: ".$mensaje;
+
+//Enviamos el mensaje y comprobamos el resultado
+if (mail($dest,$asunto,$cuerpo,$headers)) {
+echo'<script type="text/javascript">
+    alert("Mensaje Enviado Exitosamente, Nos pondremos en contacto con usted a la brevedad");
+    window.location.href="../sections#pricing";
+    </script>';
+}
+else {
+echo'<script type="text/javascript">
+    alert("Disculpe, El Mensaje No Fue Enviado Exitosamente. Intente nuevamente más tarde.");
+    window.location.href="../sections#pricing";
+    </script>';
+}
+ ?>
+
